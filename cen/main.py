d2d6290d6b569e25dc949e9623c86d8557989722
Nitin Kumar <nkumarcc@gmail.com>
2020-03-22 13:55:31 -0400

    Moved blueprint register

--- cen/main.py
+++ cen/main.py
@@ -2,7 +2,7 @@ from flask import Flask
 from app.routes import cens_blueprint
 
 app = Flask(__name__)
+app.register_blueprint(cens_blueprint)
 
 if __name__ == "__main__": 
-    app.register_blueprint(cens_blueprint)
     app.run() 
