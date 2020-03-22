a14280ea323fc272f78238bbca7aac930179e375
Nitin Kumar <nkumarcc@gmail.com>
2020-03-22 13:37:08 -0400

    Fixed main.py

--- cen/main.py
+++ cen/main.py
@@ -1,4 +1,8 @@
+from flask import Flask
 from app.routes import cens_blueprint
 
+app = Flask(__name__)
+
 if __name__ == "__main__": 
-    cens_blueprint.run() 
+    app.register_blueprint(cens_blueprint)
+    app.run() 
